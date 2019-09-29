d477822c606c8260cee2431faa7f2809d23cc037
Sean Cho <ihcho131313@gmail.com>
2019-09-29 08:19:59 -0500

    homepageTest done

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from selenium import webdriver
+import os
 
-# Create your tests here.
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.chromedriver = "chromedriver.exe"  #chromedriver should be installed and env variable shuould be setup in advance
+        os.environ["webdriver.chrome.driver"] = self.chromedriver
+        self.browser = webdriver.Chrome(self.chromedriver)
+
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('install', self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
