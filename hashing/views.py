9f4a762174f2713a4d4e30a42897a087d0d95623
Sean Cho <ihcho131313@gmail.com>
2019-10-05 22:59:44 -0500

    view change

--- hashing/views.py
+++ hashing/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from .forms import HashForm
 from .models import Hash
 import hashlib
+
 # Create your views here.
 def home(request):
+
+    if request.mothod == 'POST':
+        filled_form = HashForm(request.POST)
+        if filled_form.is_valid():
+            text = filled_form.cleaned_data['text']
+
     form = HashForm()
     return render(request, 'hashing/home.html', {'form': form})
